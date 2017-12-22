f065f0bd3495a028df36a0df48c203ec33646a15
Kovka Mykhailo <kovkamy@gmail.com>
2017-12-22 22:10:17 +0200

    pagination

--- src/App.js
+++ src/App.js
@@ -8,60 +8,28 @@ class App extends Component {
 constructor(props) {
 super(props);
 this.state=({tasks:[
-    // {
-    // "id": 1,
-    // "username": "Test User",
-    // "email": "test_user_1@example.com",
-    // "text": "Hello, world!",
-    // "status": 10,
-    // "image_path": "https://uxcandy.com/~shapoval/test-task-backend/upload/user_images/5900dfd7/1508836540_1.jpg"
-    // },
-    // {
-    //     "id": 3,
-    //     "username": "Test User 2",
-    //     "email": "test_user_2@example.com",
-    //     "text": "Hello from user 2!",
-    //     "status": 0,
-    //     "image_path": "https://uxcandy.com/~shapoval/test-task-backend/upload/user_images/5900dfd7/1508836666_3.jpg"
-    // },
-    // {
-    //     "id": 4,
-    //     "username": "Test User 3",
-    //     "email": "test_user_3@example.com",
-    //     "text": "Hello from user 3!",
-    //     "status": 0,
-    //     "image_path": "https://uxcandy.com/~shapoval/test-task-backend/upload/user_images/5900dfd7/1508836803_4.jpg"
-    // },
-    // {
-    //     "id": 5,
-    //     "username": "Test User 4",
-    //     "email": "test_user_4@example.com",
-    //     "text": "Hello from user 4!",
-    //     "status": 10,
-    //     "image_path": "https://uxcandy.com/~shapoval/test-task-backend/upload/user_images/5900dfd7/1508836803_4.jpg"
-    // }
-
-
     ],
-    filterTasks:[]
+    filterTasks:[],
+    page:1
 });
 this.makePagination= this.makePagination.bind(this);
 this.getTaskData= this.getTaskData.bind(this);
 }
 makePagination(pageNum,countPage)   {
-var filterTasks = [];
-console.log(pageNum);
-let g = pageNum === 0?pageNum:pageNum-1;
- this.state.tasks.forEach (
-     function (item,i)    {
-         if (i>=pageNum*countPage-g&&filterTasks.length<countPage)
-             filterTasks.push(item);
-                          });
-this.setState({filterTasks:filterTasks});
+//var filterTasks = [];
+// console.log(pageNum);
+// let g = pageNum === 0?pageNum:pageNum-1;
+//  this.state.tasks.forEach (
+//      function (item,i)    {
+//          if (i>=pageNum*countPage-g&&filterTasks.length<countPage)
+//              filterTasks.push(item);
+//                           });
+this.setState({page:pageNum});
+this.getTaskData(pageNum);
                                     }
-getTaskData() {
+getTaskData(page) {
     var context = this;
-    fetch('https://uxcandy.com/~shapoval/test-task-backend/?developer=Misha').
+    fetch(`https://uxcandy.com/~shapoval/test-task-backend/?developer=Misha&page=${page+1}`).
     then(function(response)
         {
             if (response.status!==200) {
@@ -69,8 +37,8 @@ getTaskData() {
             }
             response.json().then(function(data)
                 {
-                    context.setState({tasks:data.message.tasks});
-                    context.makePagination(0,3);
+                    context.setState({tasks:data.message.tasks,length:data.message.total_task_count});
+                    //context.makePagination(0,3);
                 }
             );
         }
@@ -80,11 +48,14 @@ getTaskData() {
 componentDidMount() {
 this.getTaskData();
 }
+// componentDidUpdate() {
+// this.getTaskData();
+// }
   render() {
     return (
       <div className="App">
-        <ListView tasks= {this.state.filterTasks} getTasks={this.getTaskData}/>
-        <Paginate length={this.state.tasks.length} countPage={3} paginationContext={this.makePagination}/>
+        <ListView tasks= {this.state.tasks} getTasks={this.getTaskData}/>
+        <Paginate length={this.state.length} countPage={3} paginationContext={this.makePagination}/>
 
       </div>
             );
